042f851869d315ecea3e7e6561d421326bb9c8b5
drorya2324@gmail.com <drorya2324@gmail.com>
2020-11-22 12:05:03 +0200

    added inputs titles

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,11 @@
 const { default: Axios } = require('axios')
 const express = require('express')
-const router = express.Router()
 const Transaction = require('../model/Transaction-model')
 
+const router = express.Router()
 
 const serverPort = 3001
-// router.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-//     next()
-// })
 
 router.get(`/transactions`,async function (req, res) {
     const results = await Transaction.find({})
@@ -44,21 +38,4 @@ router.delete(`/transaction`, async function (req, res) {
 
 
 
-//Dummy data,only run once!
-// const transactions=[
-//     { id:'d0',amount: 3200, vendor: "Elevation", category: "Salary" },
-//     { id:'w1',amount: -7, vendor: "Runescape", category: "Entertainment" },
-//     { id:'w2',amount: -20, vendor: "Subway", category: "Food" },
-//     { id:'w3',amount: -98, vendor: "La Baguetterie", category: "Food" }
-//   ]
-
-//   transactions.forEach(t=>{
-//     const tr = new Transaction({
-//         amount:t.amount,
-//         category:t.category,
-//         vendor:t.vendor
-//     })
-//     tr.save()
-//   })
-//
 module.exports =router
--- src/Components/Operations.js
+++ src/Components/Operations.js
@@ -25,7 +25,6 @@ class Operations extends Component {
       await this.setState({ amount: Number(`-${this.state.amount}`) })
     }
     await this.props.postTransaction(this.state)
-    console.log("Operations state", this.state)
      this.clearInputs()
      this.setState({ toRedirect: true })
   }
@@ -45,11 +44,11 @@ class Operations extends Component {
           <Redirect to={'/'} />
           :
           <div id="op-card">
-            
-
-            <input className="input" type='number' name="amount" value={this.state.amount} onChange={this.updateState} placeholder="ex. 100"></input>
-            <input className="input" name="category" value={this.state.category} onChange={this.updateState} placeholder="ex. Education"></input>
-            <input className="input" name="vendor" value={this.state.vendor} onChange={this.updateState} placeholder="ex. Elevation"></input> 
+            <span className="op-input-name amount-in"> Amount </span> <input className="input" type='number' name="amount" value={this.state.amount} onChange={this.updateState} placeholder="ex. 100"></input>
+            <br></br>
+            <span className="op-input-name category-in"> category </span> <input className="input" name="category" value={this.state.category} onChange={this.updateState} placeholder="ex. Education"></input>
+            <br></br>
+            <span className="op-input-name vendor-in"> vendor </span> <input className="input" name="vendor" value={this.state.vendor} onChange={this.updateState} placeholder="ex. Elevation"></input> 
             
             <Button  id="link-wise" onClick={async() => await this.postTransaction(false)}>Deposit</Button>
             <Button  id="link-wise" onClick={async() => await this.postTransaction(true)} >Withdraw</Button>
